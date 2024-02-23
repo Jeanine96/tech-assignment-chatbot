44c078eaa1a441af81ae23c76579d2bedd728184
Jeanine96 <jeaninederegter@gmail.com>
2024-02-23 14:27:44 +0100

    Initial Commit

---
+++ Chatbot/ChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatbotApp.swift
+//  Chatbot
+//
+//  Created by Jeanine on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
