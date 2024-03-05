c3141474613f34c5e4c0018c3b5fc10906a469a4
Jeanine96 <jeaninederegter@gmail.com>
2024-03-05 10:03:48 +0100

    Beginning of making api call to Jan ai

---
+++ Chatbot/ChatViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatViewModel.swift
+//  Chatbot
+//
+//  Created by Jeanine on 05/03/2024.
+//
+
+import Foundation
---
+++ Chatbot/ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView.swift
+//  Chatbot
+//
+//  Created by Jeanine on 05/03/2024.
+//
+
+import Foundation
